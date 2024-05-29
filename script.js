e68f1b3521a63bcb2ee70d49976d7165f93b6a5d
Pallab7980 <137693105+Pallab7980@users.noreply.github.com>
2024-05-29 23:45:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -458,12 +458,11 @@ function blank() {
 // })
 
 
+
 function loa() {
   Notification.requestPermission(()=>{
     if(Notification.permission ==="granted") {
-new Notification("Welcome to MG Laptop",{
-  icon:'logo-between-letter-m-and-g-or-mg-vector-27810742.png'
-});
+new Notification("Welcome to MG Laptop");
     }
 
     
